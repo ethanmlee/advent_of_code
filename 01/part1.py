579656be78e9969e052650430aa44018da9fc14b
Ethan M. Lee <ethan@mlee.xyz>
2023-12-01 20:03:53 -0500

    split part 1 and part 2 into different files

---
+++ 01/part1.py
@@ -0,0 +1,31 @@
+#!/bin/python
+# could I do this without importing re?
+import re
+
+# input.txt as file
+file = open('input.txt', 'r')
+
+
+# combines first and last digits of a string
+def first_last_pt1(i):
+    # find all digits and make a list
+    match = re.findall(r'\d', i)
+    # return first digit
+    first = match[0]
+    # return last digit
+    last = match[-1]
+    # append numbers together as string
+    string = str(first + last)
+    # convert to integer
+    return int(string)
+
+
+# set num to 0
+num = 0
+
+# add all the numbers up
+for line in file:
+    num = first_last_pt1(line) + int(num)
+
+# return solution to part 1
+print('part one answer:', num)
--- 01/part2.py
+++ 01/part2.py
@@ -1,40 +1,19 @@
 #!/bin/python
+# could I do this without importing re?
 import re
-
-
-# PART ONE
 
 # input.txt as file
 file = open('input.txt', 'r')
 
 
-# combines first and last digits of a string
-def first_last_pt1(i):
-    match = re.findall(r'\d', i)
-    first = match[0]
-    last = match[-1]
-    string = str(first + last)
-    return int(string)
-
-
-# set num to 0
-num = 0
-
-for line in file:
-    num = first_last_pt1(line) + int(num)
-
-print('part one answer:', num)
-
-
-# PART TWO
-# I want to die
-
+# function to reverse the string
 def reverse(string):
     string = string[::-1]
     return string
 
 
-def first_last_pt2(i):
+# and the cancer begins
+def first_last(i):
     first = re.search(r'\d|one|two|three|four|five|six|seven|eight|nine', i)[0]
     if first.isalpha():
         for word in first:
@@ -51,11 +30,12 @@ def first_last_pt2(i):
             first = first.replace("eight", "8")
             first = first.replace("nine", "9")
     i = reverse(i)
+    # holy shit wtf am I doing????
     last = re.search(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', i)[0]
     if last.isalpha():
         for word in last:
-            # solution above would also work here, except use the reverse
-            # function for each in the list
+            # comment above would also work here, except use the reverse
+            # function for each word in the list
             last = last.replace("eno", "1")
             last = last.replace("owt", "2")
             last = last.replace("eerht", "3")
@@ -65,17 +45,18 @@ def first_last_pt2(i):
             last = last.replace("neves", "7")
             last = last.replace("thgie", "8")
             last = last.replace("enin", "9")
+    # append numbers together as string
     string = str(first + last)
+    # convert to integer
     return int(string)
 
 
-# open file reset cursor to top
-file.seek(0)
-
-# reset num to zero for second part
+# set num to 0
 num = 0
 
+# add all the numbers up
 for line in file:
-    num = first_last_pt2(line) + int(num)
+    num = first_last(line) + int(num)
 
+# return solution to part 2
 print('part two asnwer:', num)
