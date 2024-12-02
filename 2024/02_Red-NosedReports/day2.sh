416b36c0c25582fbcfa0487b71665fd25947aacb
Ethan M. Lee <ethan@mlee.xyz>
2024-12-02 03:40:36 -0500

    is_safe function

--- 2024/02_Red-NosedReports/day2.sh
+++ 2024/02_Red-NosedReports/day2.sh
@@ -1,33 +1,45 @@
 #!/bin/sh
-
 silver=0
 
-while read line; do
-
+is_safe() {
+  line="$1"
   fnum=1
-  pd=0
-  safe="False"
   oldneg=""
-  while [ "$fnum" -le "$(( $(echo $line | wc -w) - 1 ))" ]; do
-    addnum=$(($fnum + 1))
+  safe="True"
+  while [ "$fnum" -le "$(( $(echo "$line" | wc -w) - 1 ))" ]; do
+    addnum=$((fnum + 1))
 
     x=$(echo "$line" | awk -v i="$fnum" '{print $i}')
     y=$(echo "$line" | awk -v i="$addnum" '{print $i}')
 
+    d=$((x - y))
 
-    d=$(( $x - $y ))
+    [ $d -lt 0 ] && { isneg="True"; d=$(( d * -1 )); } || isneg="False"
 
-    [ $d -lt 0 ] && { isneg="True"; d=$(( $d * -1 )); } || isneg="False"
-
-    [ "$oldneg" != "" ] && [ "$isneg" != "$oldneg" ] && { safe="False"; break; }
+    # not safe if isneg changes
+    [ "$oldneg" != "" ] && [ "$isneg" != "$oldneg" ] && { safe="False"; return; }
     oldneg="$isneg"
 
-    [ $d -gt 3 ] || [ $d -lt 1 ] && { safe="False"; break; } || safe="True"
-    fnum=$addnum
 
+    # not safe if diff greater 3 or less than 1
+    [ $d -gt 3 ] || [ $d -lt 1 ] && { safe="False"; return; }
+
+
+    # reset x and y for next time in loop
+    fnum=$addnum
   done
 
-  [ $safe = "True" ] && silver=$(( $silver + 1 ))
+  echo $safe
+}
+
+while read -r line; do
+  echo $line
+
+  # id safe then iterate
+  if [ "$(is_safe "$line")" = "True" ]; then
+    silver=$((silver + 1))
+    continue
+  fi
 
 done < $@
 
