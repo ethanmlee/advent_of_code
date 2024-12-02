13aae1291a1ec770adec3df68c0f3809f0b7daaf
Ethan M. Lee <ethan@mlee.xyz>
2024-12-02 01:14:54 -0500

    part1

--- 2024/02_Red-NosedReports/day2.sh
+++ 2024/02_Red-NosedReports/day2.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-while read line; do
-  #echo "$line"
+silver=0
 
+while read line; do
+  fnum=1
+  safe=1
+  while [ "$fnum" -le "$(( $(echo $line | wc -w) - 1 ))" ]; do
+    addnum=$(($fnum + 1))
+
+    x=$(echo "$line" | awk -v i="$fnum" '{print $i}')
+    y=$(echo "$line" | awk -v i="$addnum" '{print $i}')
+    
+    d=$(( $x - $y ))
+    [ $d -lt 0 ] && d=$(( $d * -1 ))
+    [ $d -ge 3 ] && safe=False && break || safe=True
+    fnum=$addnum
+  done
+
+  [ $safe = True ] && silver=$(( $silver + 1 ))
 
 done < $@
+
+echo $silver
