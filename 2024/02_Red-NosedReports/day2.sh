eb151278c961c7442fdd1830739474f7fe771b24
Ethan M. Lee <ethan@mlee.xyz>
2024-12-02 02:43:52 -0500

    fuck I got part1 wrong and commited it as if it were correct

--- 2024/02_Red-NosedReports/day2.sh
+++ 2024/02_Red-NosedReports/day2.sh
@@ -3,21 +3,31 @@
 silver=0
 
 while read line; do
+
   fnum=1
-  safe=1
+  pd=0
+  safe="False"
+  oldneg=""
   while [ "$fnum" -le "$(( $(echo $line | wc -w) - 1 ))" ]; do
     addnum=$(($fnum + 1))
 
     x=$(echo "$line" | awk -v i="$fnum" '{print $i}')
     y=$(echo "$line" | awk -v i="$addnum" '{print $i}')
-    
+
+
     d=$(( $x - $y ))
-    [ $d -lt 0 ] && d=$(( $d * -1 ))
-    [ $d -ge 3 ] && safe=False && break || safe=True
+
+    [ $d -lt 0 ] && { isneg="True"; d=$(( $d * -1 )); } || isneg="False"
+
+    [ "$oldneg" != "" ] && [ "$isneg" != "$oldneg" ] && { safe="False"; break; }
+    oldneg="$isneg"
+
+    [ $d -gt 3 ] || [ $d -lt 1 ] && { safe="False"; break; } || safe="True"
     fnum=$addnum
+
   done
 
-  [ $safe = True ] && silver=$(( $silver + 1 ))
+  [ $safe = "True" ] && silver=$(( $silver + 1 ))
 
 done < $@
 
