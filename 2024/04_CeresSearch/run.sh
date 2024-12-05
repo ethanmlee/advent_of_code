dafb54708b683c6086f90c9f4d888484a289982a
Ethan M. Lee <ethan@mlee.xyz>
2024-12-04 21:55:17 -0500

    my god this is so fucking bad

--- 2024/04_CeresSearch/run.sh
+++ 2024/04_CeresSearch/run.sh
@@ -9,12 +9,84 @@ file="$@"
 char_at () {
     line=$(sed -n "${2}p" "$file")
     char=$(echo "$line" | cut -c "$1")
-    echo $char
+    echo $char $1 $2
 }
 
+silver=0
 for y in $( seq 1 $y_bounds ); do
-
   for x in $( seq 1 $x_bounds ); do
-    char_at $x $y
+
+# well tht just fucking happened
+
+    if [ $(char_at $x $y | awk '{print $1}') = "X" ]; then
+
+      if [ $(char_at $(($x + 1 )) $y | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x + 2 )) $y | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x + 3 )) $y | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $(($x - 1 )) $y | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x - 2 )) $y | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x - 3 )) $y | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $x $(($y + 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $x $(($y + 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $x $(($y + 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $x $(($y - 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $x $(($y - 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $x $(($y - 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $(($x + 1 )) $(($y + 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x + 2 )) $(($y + 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x + 3 )) $(($y + 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $(($x - 1 )) $(($y + 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x - 2 )) $(($y + 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x - 3 )) $(($y + 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $(($x + 1 )) $(($y - 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x + 2 )) $(($y - 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x + 3 )) $(($y - 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+      if [ $(char_at $(($x - 1 )) $(($y - 1)) | awk '{print $1}') = "M" ]; then
+        if [ $(char_at $(($x - 2 )) $(($y - 2)) | awk '{print $1}') = "A" ]; then
+          if [ $(char_at $(($x - 3 )) $(($y - 3)) | awk '{print $1}') = "S" ]; then
+            silver=$(( $silver + 1 ))
+          fi
+        fi
+      fi
+
+    fi
   done
 done
+
+echo
+echo $silver
